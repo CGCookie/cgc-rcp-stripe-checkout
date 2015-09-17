21874d39434fa2d141af9f66f1c47aa3c048ca96
Jonathan Williamson <jonathan@cgcookie.com>
2015-09-17 16:06:25 -0500

    Only output shortcode if .is_active

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -18,27 +18,33 @@ function rcp_stripe_checkout_shortcode( $atts ) {
 	$user_id      = get_current_user_id();
 	$user         = get_userdata( $user_id );
 
+	$is_active    = rcp_is_active( $user_id );
+
 	$subscription = rcp_get_subscription_details( $atts['plan_id'] );
 	$price        = $subscription->price * 100;
 
+	if( !$is_active ) {
 
-	?>
-	<form action="" method="post">
-		<script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-			data-key="<?php echo $key ?>"
-			data-name="CG Cookie"
-			data-image="https://s3.amazonaws.com/cgcookie/cgc_logo_128.png"
-			data-description="Join Citizen ($<?php echo $price / 100 ?> per month)"
-			data-label="Join <?php echo $subscription->name ?>"
-			data-amount="<?php echo $price ?>"
-			data-locale="auto"
-			data-email="<?php echo $user->user_email ?>"
-			data-allow-remember-me="false"
-			>
-		</script>
-		<input type="hidden" name="subscription" value="<?php echo $subscription->id ?>" />
-		<input type="hidden" name="price" value="<?php echo $price ?>" />
-	</form>
-	<?php
+		?>
+		<form action="" method="post">
+			<script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+				data-key="<?php echo $key ?>"
+				data-name="CG Cookie"
+				data-image="https://s3.amazonaws.com/cgcookie/cgc_logo_128.png"
+				data-description="Join Citizen ($<?php echo $price / 100 ?> per month)"
+				data-label="Join <?php echo $subscription->name ?>"
+				data-amount="<?php echo $price ?>"
+				data-locale="auto"
+				data-email="<?php echo $user->user_email ?>"
+				data-allow-remember-me="false"
+				>
+			</script>
+			<input type="hidden" name="subscription" value="<?php echo $subscription->id ?>" />
+			<input type="hidden" name="price" value="<?php echo $price ?>" />
+		</form>
+		<?php
+
+	}
+
 }
 add_shortcode( 'rcp_stripe_checkout', 'rcp_stripe_checkout_shortcode' );
